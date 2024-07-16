3f82aec4015ce90da50c53f00d6680b6e0b3b90b
noritersand <noritersand@gmail.com>
2024-07-16 22:26:14 +0900

    Update

---
+++ tests/prgrms/level0/1234/test.js
@@ -0,0 +1,7 @@
+const {solution} = require('./solution');
+
+describe('suite 1234', () => {
+  test('case #1', () => {
+    expect(solution(1)).toBeTruthy();
+  });
+});
---
+++ tests/sample.test.js
@@ -0,0 +1,5 @@
+describe('Test Suite Sample', () => {
+  test('Test Case Sample', () => {
+    expect(true).toBeTruthy();
+  });
+});
