e28064b5777965ea419964ff76d29f44725a43a3
fix-a-lot <fixalot@outlook.kr>
2024-11-06 10:35:43 +0900

    tests -> __tests__

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -2,7 +2,8 @@
 const config = {
   verbose: true,
   collectCoverage: true,
-  notify: true
+  notify: true,
+  testRegex: '/__tests__/.*.test.(js|ts)$'
 };
 
 module.exports = config;
--- tests/prgrms/level1/_258712/solution.js
+++
@@ -1,5 +0,0 @@
-exports.solution = (friends, gifts) => {
-  let answer = 0;
-
-  return answer;
-};
